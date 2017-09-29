e758f29e301e78f0650f089b0080a30206656b0f
Daniel Groos <daniegr@stud.ntnu.no>
2017-09-29 18:17:55 +0200

    Extract dataset and build classifier

---
+++ load_data.py
@@ -0,0 +1,71 @@
+import tensorflow as tf
+import numpy as np
+import os
+import pandas as pd
+
+def load_data():
+
+    # Parameters
+    image_dir = os.path.join('.', 'data', 'images')#os.path.join('.', 'data','small_dataset', 'images')
+    num_examples = None
+    image_width = 256
+    image_height = 256
+
+    # Extract y values
+
+    # Read label column
+    label_column = pd.read_csv('data/train.csv', index_col=0)#pd.read_csv('data/small_dataset/train.csv', index_col=0)
+
+    # Make one hot vectors
+    labels = pd.get_dummies(label_column['species'])
+    num_examples = len(labels)
+    y = np.array(labels.as_matrix(), dtype=np.float32)#tf.convert_to_tensor(labels.as_matrix())
+
+    # Extract X values
+
+    # Create queue of image files
+    image_paths = []
+    for filename in os.listdir(image_dir):
+        image_paths.append(os.path.join(image_dir + "/" + filename))
+    image_paths = np.asarray(image_paths)
+    image_path_tensor = tf.convert_to_tensor(image_paths, dtype='string')
+    image_queue = tf.train.string_input_producer(image_path_tensor, num_epochs=None)
+
+    # Get images from files (TODO: Make more efficient)
+    X = [None for i in range(num_examples)]
+    with tf.Session() as sess:
+
+        init = tf.global_variables_initializer()
+        sess.run(init)
+        tf.train.start_queue_runners(sess=sess)
+
+        image_reader = tf.WholeFileReader()
+        for i in range(num_examples):
+            key, image_file = image_reader.read(image_queue)
+            image = tf.image.decode_jpeg(image_file)
+            resized = tf.image.resize_image_with_crop_or_pad(image, image_height, image_width)
+            X[i] = resized.eval(session=sess)
+    X = np.array(X, dtype=np.float32)# X = tf.convert_to_tensor(X)
+
+    # Make train/dev/test set
+
+    # Split dataset
+    split_value = int(num_examples * 0.8)
+    X_train = X[:split_value]
+    X_test = X[split_value:]
+    y_train = y[:split_value]
+    y_test = y[split_value:]
+    print(X_train.shape, X_test.shape, y_train.shape, y_test.shape)
+
+    return X_train, X_test, y_train, y_test
+
+# Start a new session to show example output.
+with tf.Session() as sess:
+
+    # Required to get the filename matching to run.
+    init = tf.global_variables_initializer()
+    sess.run(init)
+    tf.train.start_queue_runners(sess=sess)
+
+
+print("Successfuly terminated the program")
---
+++ model.py
@@ -0,0 +1,182 @@
+import tensorflow as tf
+import numpy as np
+import os
+import load_data as ld
+
+tf.logging.set_verbosity(tf.logging.INFO)
+
+# Model
+def cnn_model_fn(features, labels, mode):
+  input_layer = tf.reshape(features["x"], [-1, 256, 256, 1])
+
+  # Convolutional Layer #1
+  conv1 = tf.layers.conv2d(
+      inputs=input_layer,
+      filters=64,
+      kernel_size=[3, 3],
+      padding="same",
+      activation=tf.nn.relu)
+
+  # Convolutional Layer #2
+  conv2 = tf.layers.conv2d(
+      inputs=conv1,
+      filters=64,
+      kernel_size=[3, 3],
+      padding="same",
+      activation=tf.nn.relu)
+
+  # Pooling Layer #1
+  pool1 = tf.layers.max_pooling2d(
+      inputs=conv2,
+      pool_size=[2, 2],
+      strides=2)
+
+  # Convolutional Layer #3
+  conv3 = tf.layers.conv2d(
+      inputs=pool1,
+      filters=128,
+      kernel_size=[3, 3],
+      padding="same",
+      activation=tf.nn.relu)
+
+  # Convolutional Layer #4
+  conv4 = tf.layers.conv2d(
+      inputs=conv3,
+      filters=128,
+      kernel_size=[3, 3],
+      padding="same",
+      activation=tf.nn.relu)
+
+  # Pooling Layer #2
+  pool2 = tf.layers.max_pooling2d(
+      inputs=conv4,
+      pool_size=[2, 2],
+      strides=2)
+
+  # Convolutional Layer #5
+  conv5 = tf.layers.conv2d(
+      inputs=pool2,
+      filters=256,
+      kernel_size=[3, 3],
+      padding="same",
+      activation=tf.nn.relu)
+
+  # Convolutional Layer #6
+  conv6 = tf.layers.conv2d(
+      inputs=conv5,
+      filters=128,
+      kernel_size=[3, 3],
+      padding="same",
+      activation=tf.nn.relu)
+
+  # Pooling Layer #3
+  pool3 = tf.layers.max_pooling2d(
+      inputs=conv6,
+      pool_size=[2, 2],
+      strides=2)
+
+  # Convolutional Layer #7
+  conv7 = tf.layers.conv2d(
+      inputs=pool3,
+      filters=64,
+      kernel_size=[3, 3],
+      padding="same",
+      activation=tf.nn.relu)
+
+  # Convolutional Layer #8
+  conv8 = tf.layers.conv2d(
+      inputs=conv7,
+      filters=32,
+      kernel_size=[3, 3],
+      padding="same",
+      activation=tf.nn.relu)
+
+  # Pooling Layer #4
+  pool4 = tf.layers.max_pooling2d(
+      inputs=conv8,
+      pool_size=[2, 2],
+      strides=2)
+
+  # Fully-connected layer
+  pool4_flat = tf.reshape(pool4, [-1, 16 * 16 * 32])
+  dense = tf.layers.dense(inputs=pool4_flat, units=1024, activation=tf.nn.relu)
+  dropout = tf.layers.dropout(inputs=dense, rate=0.4, training=mode == tf.estimator.ModeKeys.TRAIN)
+
+  # Output Layer
+  output_layer = tf.layers.dense(inputs=dropout, units=29)
+
+  # Generate predictions (for PREDICT and EVAL mode)
+  predictions = {
+      # Generate predictions (for PREDICT and EVAL mode)
+      "classes": tf.argmax(input=output_layer, axis=1),
+      # Add `softmax_tensor` to the graph. It is used for PREDICT and by the
+      # `logging_hook`.
+      "probabilities": tf.nn.softmax(output_layer, name="softmax_tensor")
+  }
+
+  # PREDICT
+  if mode == tf.estimator.ModeKeys.PREDICT:
+    return tf.estimator.EstimatorSpec(mode=mode, predictions=predictions)
+
+  # Calculate Loss (for both TRAIN and EVAL modes)
+  onehot_labels = tf.convert_to_tensor(labels)
+  loss = tf.losses.softmax_cross_entropy(onehot_labels=onehot_labels, logits=output_layer)
+
+  # TRAIN
+  if mode == tf.estimator.ModeKeys.TRAIN:
+    optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.001) # 0.001
+    train_op = optimizer.minimize(
+        loss=loss,
+        global_step=tf.train.get_global_step())
+    return tf.estimator.EstimatorSpec(mode=mode, loss=loss, train_op=train_op)
+
+  # EVAL
+  else:
+      eval_metric_ops = {
+          "accuracy": tf.metrics.accuracy(
+              labels=labels, predictions=predictions["classes"])}
+      return tf.estimator.EstimatorSpec(mode=mode, loss=loss, eval_metric_ops=eval_metric_ops)
+
+def main(unused_argv):
+
+  # Extract data
+  X_train, X_test, y_train, y_test = ld.load_data()
+
+  # Initialize, train and test model
+
+  # Create model from estimator
+  regressor = tf.estimator.Estimator(
+      model_fn=cnn_model_fn, model_dir="tmp/cnn-test")
+
+  # Set up logging for predictions
+  # tensors_to_log = {"probabilities": "softmax_tensor"}
+  # logging_hook = tf.train.LoggingTensorHook(
+  #     tensors=tensors_to_log, every_n_iter=50)
+
+  # Train model on training set
+  train_input_fn = tf.estimator.inputs.numpy_input_fn(
+      x={"x": X_train},
+      y=y_train,
+      batch_size=2,
+      num_epochs=None,
+      shuffle=True)
+  regressor.train(
+      input_fn=train_input_fn,
+      steps=10000)
+      # hooks=[logging_hook])
+
+  print("Trained model")
+
+  # Test model on test set
+  eval_input_fn = tf.estimator.inputs.numpy_input_fn(
+      x={"x": X_test},
+      y=y_test,
+      num_epochs=1,
+      shuffle=False)
+  eval_results = regressor.evaluate(input_fn=eval_input_fn)
+  print(eval_results)
+
+  print("Tested model")
+
+if __name__ == "__main__":
+    tf.app.run()
